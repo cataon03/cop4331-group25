ae4a2889431d82d627f10fc8a08b2e4d8b59b8af
Strickland <strickland.jonathan.ext@siemens-energy.com>
2025-02-21 15:31:16 -0500

    completed basic login screen

--- web/chimpr/app/login.tsx
+++ web/chimpr/app/login.tsx
@@ -1,8 +1,15 @@
+"use client";
+
+import { Box, Button, Checkbox, FormControlLabel, TextField } from '@mui/material';
 import Logo from './components/logo';
 import BusinessMonkey from './style/img/businessman.png';
+import { useState } from 'react';
 
 
 export default function Login() {
+
+    const [showPassword, setShowPassword] = useState(false);
+
     return <>
                 <div className="main">
                     <div className="bg"></div>
@@ -13,13 +20,28 @@ export default function Login() {
                     <div className="island">
                         <div className="login-main">
                              <Logo />
-                             <h1>Login</h1>
+                            <div className='login-area'>
+                                {/* Login */}
+                                <p style={{color:'black'}} className='header-text'>Sign In</p>
+                                <TextField required id="standard-basic" style={{width: '20vw'}} label="Username" />
+                                
+                                <div style={{display: "flex", flexDirection: "column"}}>
+                                    <TextField required id="standard-basic" style={{width: '20vw'}} type={showPassword ? "" : "password"} label="Password" />
+                                    <FormControlLabel label="Show Password" control={<Checkbox onChange={() => {setShowPassword(!showPassword)}}/>}/>
+                                </div>
+
+                                <Button variant='contained' style={{width: '20vw'}}>Login</Button>
+                                
+                                {/* Or Sign Up */}
+                                <p>Don't have an account?</p> <Button>Sign Up</Button>
+                            </div>
                         </div>
                         <div className="login-side">
-                            <p className='header-text'>Welcome!</p>
                             <img src={BusinessMonkey.src}/>
-                            <p>New Here?</p>
-                            <button>Register</button>
+                            <div style={{textAlign: 'center'}}>
+                                <p className='header-text'>Welome To The Future of Recruiting</p>
+                                <p>We put the fair in career fair</p>
+                            </div>
                         </div>
                     </div>
                 </div>
