7066e7eae600b980e88b52ed25e12c5aa74cfa2e
Strickland <strickland.jonathan.ext@siemens-energy.com>
2025-02-22 17:42:26 -0500

    wip: working on student sign up

--- web/chimpr/app/components/logo.tsx
+++ web/chimpr/app/components/logo.tsx
@@ -3,9 +3,9 @@ import LogoImg from './style/img/monkey.png'
 
 export default function Logo() {
     return (
-    <div className="logo">
-        <img src={LogoImg.src}/>
-        <p className='logo-text'>ChimpR</p>
-    </div>
+        <div className="logo">
+            <img src={LogoImg.src}/>
+            <p className='logo-text'>ChimpR</p>
+        </div>
     );
 }
--- web/chimpr/app/page.tsx
+++ web/chimpr/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
-import Login from "./login";
+import Login from "./components/login";
 import './style/style.css'
+import MainIsland from "./MainIsland";
 
 export default function Home() {
   return (
-    <Login/>
+    <MainIsland/>
   );
 }
